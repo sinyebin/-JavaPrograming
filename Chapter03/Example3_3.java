e77d3da7bab57349dbdf0260c736a4936f54a3d3
sinyebin <dpqls0625@naver.com>
2024-05-12 21:24:59 +0900

    3.1 반복문

---
+++ Chapter03/Example3_3.java
@@ -0,0 +1,9 @@
+public class Example3_3 {
+    public static void main(String[] args) {
+        char c = 'a';
+        do {
+            System.out.println(c);
+            c = (char) (c + 1);
+        } while (c <= 'z');
+    }
+}
